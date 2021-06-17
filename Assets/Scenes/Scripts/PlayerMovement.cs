c3d3fd0f30ba66c1754eb6e28fb10ccfe4e0022f
natblockman <48959497+natblockman@users.noreply.github.com>
2021-06-17 12:43:27 +0700

    EP1
    PlayerMovement

--- Assets/Scenes/Scripts/PlayerMovement.cs
+++ Assets/Scenes/Scripts/PlayerMovement.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
+    private Rigidbody2D myRigidbody;
+    private Vector3 change;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        change = Vector3.zero;
+        change.x = Input.GetAxis("Horizontal");
+        change.y = Input.GetAxis("Vertical");
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+        }
+    }
+    void MoveCharacter()
+    {
+        myRigidbody.MovePosition(transform.position + change * speed * Time.deltaTime);
     }
 }
